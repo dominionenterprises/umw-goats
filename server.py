10a5f29d639b8d1565ae5a1bd7bd2286e560f6cb
Ubuntu <ubuntu@ubuntu-yakkety.localdomain>
2017-02-16 17:48:19 +0000

    new index and server

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
+import os
+import uuid
 import psycopg2
 import psycopg2.extras
+import datetime
 
-from flask import Flask, render_template, request
-app = Flask(__name__)
 
-@app.route('/')
+from datetime import datetime
+from flask import Flask, session, request
+from flask_socketio import SocketIO, emit
+from flask_socketio import join_room, leave_room
+
+app = Flask(__name__, static_url_path='')
+
+app.secret_key = os.urandom(24).encode('hex')
+
+socketio = SocketIO(app)
+
+app.route('/')
 def mainIndex():
-    return render_template('index.html')
+    # print 'in hello world'
+    return app.send_static_file('index.html')
 
+
+# start the server
 if __name__ == '__main__':
-    app.debug=True
-    app.run(host='0.0.0.0', port=8080)
+        socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
