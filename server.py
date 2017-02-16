b2c7d604a0f1c022213507ca8ee20c9f559137f5
Hunter Renard <renardhj@gmail.com>
2017-02-16 13:07:46 -0500

    creating config file

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import psycopg2
 import psycopg2.extras
+import config
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -7,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def mainIndex():
     return render_template('index.html')
+
+def connectToDB():
+    connection = 'dbname=' + psql['db'] + ' user=' + psql['user'] + ' password=' + psql['passwd']  + ' host=' + psql['host']
+    host = 'localhost'
+    print connnection
+    try:
+        return psycopg2.connect(connection)
+    except:
+        print("Can't connect to database")
 
 if __name__ == '__main__':
     app.debug=True
