fbc0caff8e034a65f279af83cf0655fad3a6ebd7
Hunter Renard <renardhj@gmail.com>
2017-02-16 13:18:06 -0500

    creating config file

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 import os
-
 import psycopg2
 import psycopg2.extras
 import config
 import datetime
-
 from datetime import datetime
 from flask import Flask, session, request, render_template
 from flask_socketio import SocketIO, emit
@@ -14,10 +12,9 @@ app.secret_key = os.urandom(24).hex()
 
 socketio = SocketIO(app)
 
-@app.route('/')
-def mainIndex():
-    # print 'in hello world'
-    return render_template('index.html')
+#------------------------------------ socket.io
+
+#------------------------------------
 
 def connectToDB():
     connection = 'dbname=' + psql['db'] + ' user=' + psql['user'] + ' password=' + psql['passwd']  + ' host=' + psql['host']
@@ -27,6 +24,11 @@ def connectToDB():
         return psycopg2.connect(connection)
     except:
         print("Can't connect to database")
+
+@app.route('/')
+def mainIndex():
+    # print 'in hello world'
+    return render_template('index.html')
 
 # start the server
 if __name__ == '__main__':
