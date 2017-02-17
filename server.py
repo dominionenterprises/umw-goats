fe6702132885bdc8064f28087124019f730cade4
Ubuntu <ubuntu@ubuntu-yakkety.localdomain>
2017-02-17 18:35:39 +0000

    final

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, Markup, request, session
 from flask_socketio import SocketIO, emit
-#from selenium.webdriver.common.by import By
+
 
 import yelp_query
 
@@ -26,40 +26,6 @@ def getVars():
 def search(query):
     emit("addResult", "result")
 
-# @app.route('/getRestaurants', methods=['GET', 'POST'])
-# def restaurants():
-#     data = [
-#             {'value':'30','content':'1 ' + Markup('<span class="fa fa-star"></span>')},
-#             {'value':'10','content':'2 ' + Markup(' <span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#             {'value':'5','content':'3 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#             {'value':'20','content':'4 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#             {'value':'40','content':'5 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')}
-#            ]
-#
-#     #
-#     # yelpsearch = yelp_query.yelper()
-#     # ratingslist = yelpsearch.resturant_search(22401)
-#     # print(type(ratingslist[0]))
-#     # print(ratingslist[0])
-#     #
-#     # # for rating in ratingslist:
-#     # #     tempList
-#     #
-#     #
-#     #
-#     # data = [
-#     #     {'value': '20','content':'1 ' + Markup('<span class="fa fa-star"></span>')},
-#     #     {'value': '20','content':'2 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#     #     {'value': '20','content':'3 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#     #     {'value': '20','content':'4 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')},
-#     #     {'value': '20','content':'5 ' + Markup('<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>')}
-#     # ]
-#
-#     # print(data[0])
-#
-#     # overall = 40
-#     location = 'Brooklyn+Bridge,New+York,NY';
-#     return render_template('charts.html')
 
 @app.route("/", methods=['GET', 'POST'])
 def mainIndex():
@@ -120,63 +86,11 @@ def mainIndex():
 
             return render_template('charts.html', data = data, overall = overall)
 
-
-
-
-            # print(type(ratingslist[0]))
-            # print(ratingslist[0])
-
-            # if 'location' not in session:
-            #     print("before category")
-            #     session['category'] = request.form['category']
-            #     pring("after category")
-            #     print(category)
         except:
-            print("ughhhhhhhhhh")
+            print("Error")
 
-
-
-
-        # print("category:")
-        # print(category)
-        # if category == "":
-        #     loc = request.form['search_term']
-        #     print("loc")
-        #     print(loc)
-
-        # if category == 'restaurant':
-        #     data = getRestaurant(location);
-        #
-        # elif category == 'shopping':
-        #     data = getShopping(location);
-        #
-        # elif category == 'nightlife':
-        #     data = getNightLife(location);
-        #
-        # elif category == 'travel':
-        #     data = getTravel(location);
-        #
-        # elif category == 'housing':
-        #     data = getHousing(location);
-        #
-        # elif category == 'safety':
-        #     data = getSafety(location);
-        #
-        # elif category == 'activites':
-        #     data = getActivites(location);
-        #
-        # else:
-
-
-
-
-
-    # qualityOfLife = getQuality(location);
-    #
-    # locationMapFormat = 'Brooklyn+Bridge,New+York,NY';
 
     return render_template('charts.html')
-# , livingQuality=qualityOfLife, buttons=buttonItems, data = data, location = locationMapFormat
 
 if __name__ == "__main__":
     socketio.run(app, host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
