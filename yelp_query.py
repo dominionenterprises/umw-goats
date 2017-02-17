42073127a923cf619e46cfff3dfb1c3d212f6cdd
Hunter Renard <renardhj@gmail.com>
2017-02-17 13:28:33 -0500

    Update yelp_query.py
    backing up

--- yelp_query.py
+++ yelp_query.py
@@ -1,55 +1,32 @@
-from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 import json
 import io
-import re
-
-import os
 
 class yelper:
+
     def __init__(self):
+
         with io.open('config_secret.json') as cred:
             creds = json.load(cred)
             auth = Oauth1Authenticator(**creds)
             self.client = Client(auth)
 
-    def resturant_search(self, query):
-        try:
-            params = {
-               'term':'food'
-            }
-            response = self.client.search(query,**params)
-        except Exception as e:
-            print(e)
-            return None
-
-        data = [0]*5
-        for resturant in response.businesses:
-            data[int(resturant.rating)-1] +=  1
-        """
-            data[business.id] = {"coordinates": (business.location.coordinate.latitude, business.location.coordinate.longitude)}
-            print(business.categories)
-            print()
-        """
-        return data
-
-y = yelper()
-
-print(y.resturant_search("22407"))
-
-class renter:
-    def __init__(self):
-        pass
-
     def search(self, query):
-        pass
+            try:
+                response = self.client.search(query)
+            except Exception as e:
+                print(e)
+                return None
 
-class zillower:
-    def search(self, query):
-        address, zipcode = query.rsplit(" ", 1)
+            data = {}
+            for business in response.businesses:
 
-        data = ZillowWrapper("X1-ZWz19cu0z5xn9n_17jma")
-        response = data.get_deep_search_results(address, zipcode)
+                data[business.id] = {"coordinates": (business.location.coordinate.latitude, business.location.coordinate.longitude)}
+                for i in dir(business):
+                    print(i, getattr(business, i))
+                break
+            return data
 
-        return GetDeepSearchResults(response)
+y =  yelper()
+print(y.search("Norfolk"))
